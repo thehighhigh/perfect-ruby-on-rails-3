9ec9147162de13116913c34f2fdb2ce125ddef64
yuto takahashi <Yuto.ld.1017@gmail.com>
2021-06-01 23:35:00 +0900

    current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+    user = User.find_by(id: session[:user_id])
+    @current_user ||= user
   end
 
   def authenticate
